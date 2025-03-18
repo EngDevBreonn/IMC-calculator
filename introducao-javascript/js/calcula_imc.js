cc91a4ac733073ab65495e9898b6f0ea190082bf
Bruno <eng.dev.breonn@gmail.com>
2025-03-18 15:28:18 -0300

    refactor: improve code structure and add BMI calculation function

--- introducao-javascript/js/calcula_imc.js
+++ introducao-javascript/js/calcula_imc.js
@@ -1,47 +1,41 @@
-var pacientes = document.querySelectorAll(".paciente");//array
-
-for (let i = 0; i < pacientes.length; i++) {
+// Cálculo do IMC do paciente
+var pacientes = document.querySelectorAll(".paciente");
+ 
+for (var i = 0; i < pacientes.length; i++) {
     const paciente = pacientes[i];
-
     var tdPeso = paciente.querySelector(".info-peso");
-    var peso = tdPeso.textContent;
-
     var tdAltura = paciente.querySelector(".info-altura");
-    var altura = tdAltura.textContent;
-
+ 
     var tdImc = paciente.querySelector(".info-imc");
-
-    //Validações de peso e altura
-
-    var pesoValido = true;//bool
-    var alturaValida = true;
-
-    if (peso <= 0 || peso > 635) {
-        console.log("Peso inválido");
-        pesoValido = false;
-        tdImc.textContent = "Peso inválido!";
-        //paciente.style.backgroundColor = "lightcoral";
-        paciente.classList.add("paciente-invalido")
+ 
+    var alturaEhValida = true;
+    var pesoEhValido = true;
+ 
+    var peso = tdPeso.textContent;
+    if (peso <= 0 || peso > 600) {
+        console.log("Peso inválido!");
+        pesoEhValido = false;
+        tdImc.textContent = "Peso inválido!"
     }
-
-    if (altura <= 0 || altura > 3.0) {
-        console.log("Altura inválido");
-        alturaValida = false;
+ 
+    var altura = tdAltura.textContent;
+    if (altura <= 0 || altura > 3) {
+        console.log("Altura inválida!");
+        alturaEhValida = false;
         tdImc.textContent = "Altura inválida!";
-        paciente.classList.add("paciente-invalido")
     }
-
-    if (!alturaValida && !pesoValido) {
-        tdImc.textContent = "Peso e Altura inválidos!";
+ 
+    if (!pesoEhValido && !alturaEhValida) {
+        tdImc.textContent = "Altura e/ou peso inválidos!"
     }
-
-    if (alturaValida && pesoValido) {
+ 
+    if (alturaEhValida && pesoEhValido) {
         var imc = calculaImc(peso, altura);
         tdImc.textContent = imc;
     }
 }
-
-function calculaImc(peso, altura){
+ 
+function calculaImc(peso, altura) {
     var imc = 0;
     imc = peso / Math.pow(altura, 2);
     return imc.toFixed(2);
